785294008e3d244bf2ce20cdfff38b37969c457c
LaoMaaa <jacky_hoalam@outlook.com>
2023-06-03 22:30:33 +1000

    Commit1

--- src/test/java/model/AddStudent.java
+++ src/test/java/model/AddStudent.java
@@ -2,11 +2,13 @@ package model;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.util.Date;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+import java.util.Calendar;
 
 /**
  *  Implement and test {Programme.addStudent } that respects the considtion given the assignment specification
@@ -18,7 +20,56 @@ import java.util.Date;
  */
 public class AddStudent {
 	
-	
+	private Programme programme;
+    private Student student;
 
+    @BeforeEach
+    public void initialise() {
+        // Initialise instances of all objects
+        programme = new Programme();
+        student = new Student("Jacky", 3896673);
+
+        // set start date of programme
+        Calendar startdate = Calendar.getInstance();
+        startdate.set(2023, Calendar.JULY, 18);
+        programme.setStartDate(startdate.getTime());
+    
+    }
+
+    @Test
+    public void testEnrollStudent_WhenProgrammeStartDateHasPassed() {
+
+        // Test to see if the start date has already passed
+        Calendar startdate = Calendar.getInstance();
+        startdate.set(2023, Calendar.JANUARY, 17);
+        programme.setStartDate(startdate.getTime());
+
+        assertEquals(false, programme.addStudent(student));
+    }
+
+    @Test
+    public void testEnrollStudent_WhenProgrammeIsFull() {
+
+        // Test to see if the programme is full
+        for(int i = 0; i < 250; i++) {
+            programme.addStudent(new Student("Student" + (i+2), i+2));
+        }
+        assertFalse(programme.addStudent(student));
+    }
+
+    @Test
+    public void testEnrollStudent_WhenStudentIsAlreadyEnrolled() {
+
+        // Test to see if student is already enrolled in the programme
+        programme.addStudent(student);
+        assertThrows(IllegalStudentEnrollException.class, () -> programme.addStudent(student));
+    }
+
+    @Test
+    public void testEnrollStudent_WhenAllConditionsAreMet() {
+
+        assertTrue(programme.addStudent(student));
+        assertEquals(1, programme.getEnrollments().size());
+    }
 
 }
